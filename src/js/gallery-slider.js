ac5b3d36d74d19a5da2afcac7b630b6bae7fa002
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-09-30 14:39:18 +0100

    update

---
+++ src/js/gallery-slider.js
@@ -0,0 +1,11 @@
+$('.gallery__list').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 4000,
+    autoplaySpeed:2000,
+    arrows: false,
+    cssEase: 'linear',
+    infinite: true,
+    fade: true,
+  });
---
+++ src/js/reviews.js
@@ -0,0 +1,13 @@
+$('.reviews-list').slick({
+  dots: true,
+
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  arrows: false,
+  autoplay: true,
+  dotsClass: 'comment__choise > li',
+}
+);
+
